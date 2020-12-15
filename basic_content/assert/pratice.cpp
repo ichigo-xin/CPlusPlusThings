f977e6c47e037df5d57d8c321586998f4564d049
chenkai <383819170@qq.com>
2020-12-15 17:29:45 +0800

    学习完基础部分

--- basic_content/assert/pratice.cpp
+++ basic_content/assert/pratice.cpp
@@ -1,15 +1,35 @@
-#define NDEBUG
+/**
+* @file using_typedef.cpp
+* @brief g++ -o using_typedef using_typedef.cpp -std=c++11
+* 取代typedef，使用using来定义别名
+* @author 光城
+* @version v1
+* @date 2019-08-07
+*/
+
 #include <iostream>
-
-#include <assert.h>
-
+#include <vector>
 using namespace std;
+
+typedef vector<int> V1;
+using V2 = vector<int>;
+
 
 int main()
 {
-    int x = 1;
-    assert(x ==6);
-    cout << "soga" <<endl;
+	int nums1[] = { 1,2,3,4,5,6 };
+	V1 vec1(nums1, nums1 + sizeof(nums1) / sizeof(int));
+	int nums2[] = { 5,7,6 };
+	V2 vec2(nums2, nums2 + sizeof(nums2) / sizeof(int));
+
+	for (auto i : vec1)
+		cout << i << " ";
+	cout << endl;
+
+	for (auto i : vec2)
+		cout << i << " ";
+	cout << endl;
+
 	system("pause");
-    return 0;
+	return 0;
 }
